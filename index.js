c189ceee4f1e2222d34a6757b89e4cefbcdd61eb
Elijah Kamanga <elijahkamanga1@gmail.com>
2025-07-05 18:18:40 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution here
+const drivers=[
+    {
+        name:'natasha',
+        hometown:'new york'
+    },
+    {
+        name:'sophia',
+        hometown:'turkey'
+    },
+    {
+        name:'Natasha',
+        hometown:'kentucky'
+    },
+    {
+        name:'leslie',
+        hometown:'cairo'
+    }
+]
+
+function findMatching(theArray, matchingName){
+    return theArray.filter(driver=> driver.toLowerCase()===matchingName.toLowerCase())
+}
+
+console.log(findMatching(drivers, 'natasha'))
+
+function fuzzyMatch(theArray, beginsWith){
+    return theArray.filter(driver=>driver.toLowerCase().startsWith(beginsWith.toLowerCase()))
+}
+console.log(fuzzyMatch(drivers, 'L'))
+
+function matchName(arrayOfDriver, matches){
+    return arrayOfDriver.filter(driver=> driver.name===matches)
+}
+
+console.log(matchName(drivers, 'leslie'))
